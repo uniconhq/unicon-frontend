489f3c666930e3d29227a8db285db30d4d825934
seeleng <seelengxd@gmail.com>
2025-01-03 10:36:09 +0800

    fix(output-metadata): make control uneditable

---
+++ src/components/node-graph/components/step/metadata/output-metadata-row.tsx
@@ -0,0 +1,177 @@
+import { Delete, Plus, Trash } from "lucide-react";
+
+import { OutputSocket } from "@/api";
+import { Button } from "@/components/ui/button";
+import { Checkbox } from "@/components/ui/checkbox";
+import {
+  Select,
+  SelectContent,
+  SelectItem,
+  SelectTrigger,
+  SelectValue,
+} from "@/components/ui/select";
+import { TableCell, TableRow } from "@/components/ui/table";
+
+import { NodeSlot } from "../../node-slot";
+import NodeInput from "../node-input";
+
+type OwnProps = {
+  socket: OutputSocket;
+  onUpdateSocketMetadata: (newMetadata: Partial<OutputSocket>) => void;
+  onEditSocketId: (oldSocketId: string) => (newSocketId: string) => void;
+  onDeleteSocket: () => void;
+  isEditable: boolean;
+};
+
+const OutputMetadataRow: React.FC<OwnProps> = ({
+  socket,
+  onUpdateSocketMetadata,
+  onEditSocketId,
+  onDeleteSocket,
+  isEditable,
+}) => {
+  return (
+    <TableRow key={socket.id}>
+      <TableCell>
+        <NodeSlot
+          key={socket.id}
+          id={socket.id}
+          label=""
+          type="target"
+          hideLabel
+        />
+      </TableCell>
+      <TableCell>
+        {isEditable ? (
+          <NodeInput value={socket.id} onChange={onEditSocketId(socket.id)} />
+        ) : (
+          <span>{socket.id}</span>
+        )}
+      </TableCell>
+      <TableCell>
+        {isEditable ? (
+          <NodeInput
+            value={socket.user_label || ""}
+            onChange={(newLabel) => {
+              onUpdateSocketMetadata({
+                user_label: newLabel,
+              });
+            }}
+          />
+        ) : (
+          <span>{socket.user_label}</span>
+        )}
+      </TableCell>
+      <TableCell>
+        <div className="flex items-center gap-2">
+          {socket.comparison ? (
+            isEditable ? (
+              <>
+                <Select>
+                  <SelectTrigger className="h-fit w-fit p-1">
+                    <SelectValue placeholder={socket.comparison?.operator} />
+                  </SelectTrigger>
+                  <SelectContent
+                    onSelect={(value) =>
+                      onUpdateSocketMetadata({
+                        comparison: {
+                          ...(socket.comparison ?? {
+                            operator: "=",
+                            value: "",
+                          }),
+                          value,
+                        },
+                      })
+                    }
+                  >
+                    <SelectItem value="=">=</SelectItem>
+                    <SelectItem value="<">&lt;</SelectItem>
+                    <SelectItem value=">">&gt;</SelectItem>
+                  </SelectContent>
+                </Select>{" "}
+                <NodeInput
+                  value={JSON.stringify(socket.comparison?.value ?? "")}
+                  onChange={(newValue) => {
+                    onUpdateSocketMetadata({
+                      comparison: {
+                        ...(socket.comparison ?? {
+                          operator: "=",
+                          value: "",
+                        }),
+                        value: JSON.parse(newValue),
+                      },
+                    });
+                  }}
+                />
+                <Button
+                  size={"sm"}
+                  className="h-fit w-fit px-1 py-1"
+                  variant={"secondary"}
+                  onClick={() =>
+                    onUpdateSocketMetadata({
+                      comparison: null,
+                    })
+                  }
+                  type="button"
+                >
+                  <Delete className="h-2 w-2" />
+                </Button>
+              </>
+            ) : (
+              <span>
+                {socket.comparison.operator}{" "}
+                {JSON.stringify(socket.comparison.value)}
+              </span>
+            )
+          ) : (
+            isEditable && (
+              <Button
+                size={"sm"}
+                className="h-fit w-fit px-1 py-1"
+                variant={"secondary"}
+                onClick={() =>
+                  onUpdateSocketMetadata({
+                    comparison: {
+                      operator: "=",
+                      value: "",
+                    },
+                  })
+                }
+                type="button"
+              >
+                <Plus className="h-2 w-2" />
+              </Button>
+            )
+          )}
+        </div>
+      </TableCell>
+      <TableCell className="flex items-center justify-center">
+        <Checkbox
+          className="inline h-4 w-4 rounded-sm border border-gray-500/50"
+          checked={socket.public || false}
+          onCheckedChange={() => {
+            onUpdateSocketMetadata({
+              public: !socket.public,
+            });
+          }}
+          disabled={!isEditable}
+        ></Checkbox>
+      </TableCell>
+      <TableCell>
+        {isEditable && (
+          <Button
+            size={"sm"}
+            className="h-fit w-fit px-1 py-1"
+            variant={"secondary"}
+            onClick={onDeleteSocket}
+            type="button"
+          >
+            <Trash className="h-2 w-2" />
+          </Button>
+        )}
+      </TableCell>
+    </TableRow>
+  );
+};
+
+export default OutputMetadataRow;
--- src/components/node-graph/components/step/metadata/output-metadata.tsx
+++ src/components/node-graph/components/step/metadata/output-metadata.tsx
@@ -1,20 +1,11 @@
-import { Delete, Plus, Trash } from "lucide-react";
+import { Plus } from "lucide-react";
 import { useCallback, useContext } from "react";
 
 import { OutputSocket, OutputStep } from "@/api";
 import { Button } from "@/components/ui/button";
-import { Checkbox } from "@/components/ui/checkbox";
-import {
-  Select,
-  SelectContent,
-  SelectItem,
-  SelectTrigger,
-  SelectValue,
-} from "@/components/ui/select";
 import {
   Table,
   TableBody,
-  TableCell,
   TableHead,
   TableHeader,
   TableRow,
@@ -23,10 +14,10 @@ import {
   GraphContext,
   GraphDispatchContext,
 } from "@/features/problems/components/tasks/graph-context";
+import { isControlSocket } from "@/utils/socket";
 
-import { NodeSlot } from "../../node-slot";
-import NodeInput from "../node-input";
 import OutputTable from "../output-table/output-table";
+import OutputMetadataRow from "./output-metadata-row";
 
 type OwnProps = {
   step: OutputStep;
@@ -36,21 +27,20 @@ const OutputMetadata: React.FC<OwnProps> = ({ step }) => {
   const { isEditing } = useContext(GraphContext)!;
   const dispatch = useContext(GraphDispatchContext)!;
 
-  const updateSocketMetadata = (
-    metadataIndex: number,
-    newMetadata: OutputSocket,
-  ) => {
-    // eslint-disable-next-line @typescript-eslint/no-unused-vars
-    const { id: _, ...newMetadataWithoutId } = newMetadata;
-    dispatch({
-      type: "UPDATE_STEP_SOCKET",
-      stepId: step.id,
-      oldSocketId: step.inputs[metadataIndex].id,
-      newSocketId: newMetadata.id,
-      isInput: true,
-      socketFields: newMetadataWithoutId,
-    });
-  };
+  // Does not handle updating socket ids. See handleEditSocketId for that
+  const updateSocketMetadata =
+    (metadataIndex: number) => (newMetadata: Partial<OutputSocket>) => {
+      // eslint-disable-next-line @typescript-eslint/no-unused-vars
+      const { id: _, ...newMetadataWithoutId } = newMetadata;
+      dispatch({
+        type: "UPDATE_STEP_SOCKET",
+        stepId: step.id,
+        oldSocketId: step.inputs[metadataIndex].id,
+        newSocketId: step.inputs[metadataIndex].id,
+        isInput: true,
+        socketFields: newMetadataWithoutId,
+      });
+    };
 
   const handleEditSocketId = (oldSocketId: string) => (newSocketId: string) => {
     dispatch({
@@ -99,137 +89,14 @@ const OutputMetadata: React.FC<OwnProps> = ({ step }) => {
           </TableHeader>
           <TableBody>
             {step.inputs.map((socket, index) => (
-              <TableRow key={socket.id}>
-                <TableCell>
-                  <NodeSlot
-                    key={socket.id}
-                    id={socket.id}
-                    label=""
-                    type="target"
-                    hideLabel
-                  />
-                </TableCell>
-                <TableCell>
-                  <NodeInput
-                    value={socket.id}
-                    onChange={handleEditSocketId(socket.id)}
-                  />
-                </TableCell>
-                <TableCell>
-                  <NodeInput
-                    value={socket.user_label || ""}
-                    onChange={(newLabel) => {
-                      updateSocketMetadata(index, {
-                        ...socket,
-                        user_label: newLabel,
-                      });
-                    }}
-                  />
-                </TableCell>
-                <TableCell>
-                  <div className="flex items-center gap-2">
-                    {socket.comparison ? (
-                      <>
-                        <Select>
-                          <SelectTrigger className="h-fit w-fit p-1">
-                            <SelectValue
-                              placeholder={socket.comparison?.operator}
-                            />
-                          </SelectTrigger>
-                          <SelectContent
-                            onSelect={(value) =>
-                              updateSocketMetadata(index, {
-                                ...socket,
-                                comparison: {
-                                  ...(socket.comparison ?? {
-                                    operator: "=",
-                                    value: "",
-                                  }),
-                                  value,
-                                },
-                              })
-                            }
-                          >
-                            <SelectItem value="=">=</SelectItem>
-                            <SelectItem value="<">&lt;</SelectItem>
-                            <SelectItem value=">">&gt;</SelectItem>
-                          </SelectContent>
-                        </Select>{" "}
-                        <NodeInput
-                          value={JSON.stringify(socket.comparison?.value ?? "")}
-                          onChange={(newValue) => {
-                            updateSocketMetadata(index, {
-                              ...socket,
-                              comparison: {
-                                ...(socket.comparison ?? {
-                                  operator: "=",
-                                  value: "",
-                                }),
-                                value: JSON.parse(newValue),
-                              },
-                            });
-                          }}
-                        />
-                        <Button
-                          size={"sm"}
-                          className="h-fit w-fit px-1 py-1"
-                          variant={"secondary"}
-                          onClick={() =>
-                            updateSocketMetadata(index, {
-                              ...socket,
-                              comparison: null,
-                            })
-                          }
-                          type="button"
-                        >
-                          <Delete className="h-2 w-2" />
-                        </Button>
-                      </>
-                    ) : (
-                      <Button
-                        size={"sm"}
-                        className="h-fit w-fit px-1 py-1"
-                        variant={"secondary"}
-                        onClick={() =>
-                          updateSocketMetadata(index, {
-                            ...socket,
-                            comparison: {
-                              operator: "=",
-                              value: "",
-                            },
-                          })
-                        }
-                        type="button"
-                      >
-                        <Plus className="h-2 w-2" />
-                      </Button>
-                    )}
-                  </div>
-                </TableCell>
-                <TableCell className="flex items-center justify-center">
-                  <Checkbox
-                    className="inline h-4 w-4 rounded-sm border border-gray-500/50"
-                    checked={socket.public || false}
-                    onCheckedChange={() => {
-                      updateSocketMetadata(index, {
-                        ...socket,
-                        public: !socket.public,
-                      });
-                    }}
-                  ></Checkbox>
-                </TableCell>
-                <TableCell>
-                  <Button
-                    size={"sm"}
-                    className="h-fit w-fit px-1 py-1"
-                    variant={"secondary"}
-                    onClick={deleteSocket(socket.id)}
-                    type="button"
-                  >
-                    <Trash className="h-2 w-2" />
-                  </Button>
-                </TableCell>
-              </TableRow>
+              <OutputMetadataRow
+                key={socket.id}
+                socket={socket}
+                onUpdateSocketMetadata={updateSocketMetadata(index)}
+                onEditSocketId={handleEditSocketId}
+                onDeleteSocket={deleteSocket(socket.id)}
+                isEditable={!isControlSocket(socket)}
+              />
             ))}
           </TableBody>
         </Table>
